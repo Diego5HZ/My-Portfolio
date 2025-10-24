a96161e4a48aaed5b781e8917cb397d729bc07ca
DiegouHZ5 <safairus@gmail.com>
2025-10-24 23:18:30 +0300

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://diego5hz.github.io/',
+  site: 'https://diego5hz.github.io',
   base: '/My-Portfolio/',
   integrations: [tailwind({
     applyBaseStyles: true
