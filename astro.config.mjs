cf551bd1b1f47fb27a3a69d418f5bfcdbacee724
DiegouHZ5 <safairus@gmail.com>
2025-10-24 22:14:07 +0300

    correccion en despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://diego5hz.github.io',
-  base: '/My-Portfolio/',
+  site: 'https://diego5hz.github.io/My-Portfolio',
+  base: '/',
   integrations: [tailwind({
     applyBaseStyles: true
   })],
